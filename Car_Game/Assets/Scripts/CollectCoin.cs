40fc40b07f4808709b7b22146d81ef18fbddb189
Adrian <abischoff8307@gmail.com>
2024-06-21 11:02:05 +0200

    added coins

--- Car_Game/Assets/Scripts/CollectCoin.cs
+++ Car_Game/Assets/Scripts/CollectCoin.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectCoins : MonoBehaviour
+public class CollectCoin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if  (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
